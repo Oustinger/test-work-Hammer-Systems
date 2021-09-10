1d9bcb6ebeacd09871a9d8e1d1566ae4ef0940f0
Oustinger <oustinger@gmail.com>
2021-09-11 00:53:41 +0500

    Add content on edit client profile page

---
+++ src/views/app-views/commons/clients/clientsList/editClient/EditClientForm.js
@@ -0,0 +1,165 @@
+import { Button, Col, Form, Input, Row } from 'antd';
+import { ROW_GUTTER } from 'constants/ThemeConstant';
+import React from 'react';
+
+const EditClientForm = ({
+  name,
+  username,
+  email,
+  phone,
+  website,
+  addressCity,
+  addressStreet,
+  addressSuite,
+  addressZipcode,
+  addressGeoLat,
+  addressGeoLng,
+  companyName,
+  companyCatchPhrase,
+  companyBs,
+  onFinish,
+  onFinishFailed
+}) => (
+  <div className="mt-4">
+    <Form
+      name="basicInformation"
+      layout="vertical"
+      initialValues={{
+        name,
+        username,
+        email,
+        phone,
+        website,
+        addressCity,
+        addressStreet,
+        addressSuite,
+        addressZipcode,
+        addressGeoLat,
+        addressGeoLng,
+        companyName,
+        companyCatchPhrase,
+        companyBs,
+      }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Row>
+        <Col xs={24} sm={24} md={24} lg={16}>
+          <Row gutter={ROW_GUTTER}>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item
+                label="Name"
+                name="name"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your name!',
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item
+                label="Username"
+                name="username"
+                rules={[
+                  {
+                    required: true,
+                    message: 'Please input your username!',
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item
+                label="Email"
+                name="email"
+                rules={[
+                  {
+                    required: true,
+                    type: 'email',
+                    message: 'Please enter a valid email!',
+                  },
+                ]}
+              >
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item label="Phone Number" name="phone">
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item label="Website" name="website">
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item label="City" name="addressCity">
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item label="Street" name="addressStreet">
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item label="Suite" name="addressSuite">
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item label="Zipcode" name="addressZipcode">
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={24}>
+              <h3>Geolocation</h3>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item label="Geo latitude" name="addressGeoLat">
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item label="Geo longitude" name="addressGeoLng">
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={24}>
+              <h3>Company</h3>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item label="Name" name="companyName">
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item label="Catch phrase" name="companyCatchPhrase">
+                <Input />
+              </Form.Item>
+            </Col>
+            <Col xs={24} sm={24} md={12}>
+              <Form.Item label="Bs" name="companyBs">
+                <Input />
+              </Form.Item>
+            </Col>
+          </Row>
+          <Button type="primary" htmlType="submit">
+            Save Change
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  </div>
+);
+
+export default EditClientForm;
--- src/views/app-views/commons/clients/clientsList/editClient/index.js
+++ src/views/app-views/commons/clients/clientsList/editClient/index.js
@@ -1,5 +1,133 @@
-const EditClient = () => {
-  return <></>;
-};
+import { message } from 'antd';
+import Loading from 'components/shared-components/Loading';
+import { APP_PREFIX_PATH } from 'configs/AppConfig';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'redux';
+import { getClientsData } from 'redux/actions/App';
+import EditClientForm from './EditClientForm';
 
-export default EditClient;
+class EditClient extends Component {
+  constructor(props) {
+    super(props);
+
+    this.clientId = parseInt(this.props.match.params.clientId);
+
+    if (this.clientId) {
+      if (this.clientsData === null) {
+        this.props.getClientsData();
+      } else {
+        this.client = this.props.clientsData.find((client) => client.id === this.clientId);
+      }
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.showError) {
+      setTimeout(() => {
+        this.props.hideAppError();
+      }, 3000);
+    }
+
+    if (this.clientId && !this.client)
+      this.client = this.props.clientsData.find((client) => client.id === this.clientId);
+  }
+
+  componentDidUpdate() {
+    if (this.props.showError) {
+      setTimeout(() => {
+        this.props.hideAppError();
+      }, 3000);
+    }
+
+    if (this.clientId && !this.client)
+      this.client = this.props.clientsData.find((client) => client.id === this.clientId);
+  }
+
+  render() {
+    const onFinish = (values) => {
+      const data = {
+        name: values.name,
+        username: values.username,
+        email: values.email,
+        phone: values.phone,
+        website: values.website,
+        address: {
+          city: values.addressCity,
+          street: values.addressStreet,
+          suite: values.addressSuite,
+          zipcode: values.addressZipcode,
+          geo: {
+            lat: values.addressGeoLat,
+            lgn: values.addressGeoLng,
+          },
+        },
+        company: {
+          name: values.name,
+          catchPhrase: values.catchPhrase,
+          bs: values.bs,
+        },
+      };
+
+      this.props.setNewClientData(data);
+
+      const key = 'updatable';
+      message.loading({ content: 'Updating...', key });
+      setTimeout(() => {
+        message.success({ content: 'Done!', key, duration: 2 });
+        this.props.history.push(`${APP_PREFIX_PATH}/commons/clients/list`);
+      }, 1000);
+    };
+
+    const onFinishFailed = (errorInfo) => {
+      console.log('Failed:', errorInfo);
+    };
+
+    return (
+      <>
+        {this.props.isLoading ? (
+          <Loading cover="content" />
+        ) : !this.clientId ? (
+          <h2>Error: Client ID not specified</h2>
+        ) : !this.client ? (
+          <h2>Error: Client ID is not correct</h2>
+        ) : (
+          <EditClientForm
+            name={this.client.name}
+            username={this.client.username}
+            email={this.client.email}
+            phone={this.client.phone}
+            website={this.client.website}
+            addressCity={this.client.address.city}
+            addressStreet={this.client.address.street}
+            addressSuite={this.client.address.suite}
+            addressZipcode={this.client.address.zipcode}
+            addressGeoLat={this.client.address.geo.lat}
+            addressGeoLng={this.client.address.geo.lng}
+            companyName={this.client.company.name}
+            companyCatchPhrase={this.client.company.catchPhrase}
+            companyBs={this.client.company.bs}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+          />
+        )}
+      </>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isLoading: state.app.isLoading,
+  clientsData: state.app.clientsData,
+  showError: state.app.showError,
+  errorMessage: state.app.errorMessage,
+});
+
+export default compose(
+  connect(mapStateToProps, {
+    setNewClientData: () => ({ type: 'asd' }),
+    getClientsData,
+  }),
+  withRouter
+)(EditClient);
