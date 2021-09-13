a871915cbef7439c038ea79d47d46d21b1369843
Oustinger <oustinger@gmail.com>
2021-09-13 17:58:48 +0500

    Updates

---
+++ static/js/13.4bbf345b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpemilus=this.webpackJsonpemilus||[]).push([[13],{499:function(e,s,n){"use strict";n.r(s);n(0);var t=n(3);s.default=function(){return Object(t.jsx)("div",{children:"Home component works!"})}}}]);
+//# sourceMappingURL=13.4bbf345b.chunk.js.map
